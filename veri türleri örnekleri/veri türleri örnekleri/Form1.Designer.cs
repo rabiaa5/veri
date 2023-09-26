5c38b236032c00ba21045f1831afe5a7fc5b854c
rabiaa5 <145457440+rabiaa5@users.noreply.github.com>
2023-09-26 14:08:14 +0300

    Add files via upload

---
+++ veri türleri örnekleri/veri türleri örnekleri/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace veri_türleri_örnekleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblSonuç = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(85, 86);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "hesapla ";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblSonuç
+            // 
+            this.lblSonuç.AutoSize = true;
+            this.lblSonuç.Location = new System.Drawing.Point(327, 96);
+            this.lblSonuç.Name = "lblSonuç";
+            this.lblSonuç.Size = new System.Drawing.Size(27, 13);
+            this.lblSonuç.TabIndex = 1;
+            this.lblSonuç.Text = "*****";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(197, 89);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 209);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.lblSonuç);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblSonuç;
+        private System.Windows.Forms.TextBox txtSayi;
+    }
+}
+
